c9f5b42cff35900ce6ae497ac999942e54b7a30a
Kappa322 <Krappa322@users.noreply.github.com>
2021-04-05 01:04:54 +0200

    maximize mock window on startup

--- arcdps_mock/main.cpp
+++ arcdps_mock/main.cpp
@@ -68,7 +68,7 @@ int Run(const char* pModulePath, const char* pMockFilePath)
 	//ImGui_ImplWin32_EnableDpiAwareness();
 	WNDCLASSEX wc = { sizeof(WNDCLASSEX), CS_CLASSDC, WndProc, 0L, 0L, GetModuleHandle(NULL), NULL, NULL, NULL, NULL, _T("Arcdps Mock"), NULL };
 	::RegisterClassEx(&wc);
-	HWND hwnd = ::CreateWindow(wc.lpszClassName, _T("Arcdps Mock"), WS_OVERLAPPEDWINDOW, 100, 100, 1280, 800, NULL, NULL, wc.hInstance, NULL);
+	HWND hwnd = ::CreateWindow(wc.lpszClassName, _T("Arcdps Mock"), WS_OVERLAPPEDWINDOW | WS_MAXIMIZE, 100, 100, 1280, 800, NULL, NULL, wc.hInstance, NULL);
 
 	// Initialize Direct3D
 	if (!CreateDeviceD3D(hwnd))
